79d86323982d7838f881c719ed9b573fd1f3a220
AbdiazizM3 <abdiazizmohamed333555@gmail.com>
2025-04-25 19:52:29 +0100

    updated the connection.js file

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,4 @@
+const { config } = require("dotenv");
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
 
@@ -8,9 +9,6 @@ require("dotenv").config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
-
-const config = {};
-
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
